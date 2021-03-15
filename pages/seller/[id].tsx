7e5541e6a25b39565af085ab1ae64ae0f3ea7cfd
Jay Shukla <jay.shukla@bolttech.io>
2021-03-15 19:16:54 +0800

    Added tailwind css config and login page

---
+++ pages/seller/[id].tsx
@@ -0,0 +1,91 @@
+import React, { useState, FunctionComponent } from "react";
+import { GetStaticPaths, GetStaticProps } from "next";
+import productData from "../../data/products.json";
+// import axios from "axios";
+
+import Header from "../../components/Header/Header";
+import Products from "../../components/Products/Products";
+import Footer from "../../components/Footer/Footer";
+import QuickView from "../../components/QuickView/QuickView";
+
+import { ProductWeb, QuickPreview } from "../../context/ShoppingCart";
+
+const Items: FunctionComponent<{ products: ProductWeb[] }> = ({ products }) => {
+  const [term, setTerm] = useState<string>("");
+
+  const [modalActive, flipModelState] = useState<boolean>(false);
+
+  const initQuickPreview = {
+    image: "blank",
+    id: 0,
+    price: 0,
+    name: "blank",
+  };
+
+  const [quickViewProduct, setQuickViewProduct] = useState<QuickPreview>(
+    initQuickPreview
+  );
+
+  // Search by Keyword
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTerm(e.target.value);
+  };
+
+  // Mobile Search Reset
+  const resetSearch = () => {
+    setTerm("");
+  };
+
+  // Open Modal
+  const openModal = (product: QuickPreview) => {
+    setQuickViewProduct(product);
+    flipModelState(true);
+  };
+
+  // Close Modal
+  const closeModal = () => {
+    flipModelState(false);
+  };
+
+  return (
+    <div className="container">
+      <Header
+        handleSearch={handleSearch}
+        resetSearch={resetSearch}
+        searchValue={term}
+      />
+      <Products
+        productsList={products}
+        searchTerm={term}
+        openModal={openModal}
+      />
+      <Footer />
+      <QuickView
+        product={quickViewProduct}
+        openModalState={modalActive}
+        closeModal={closeModal}
+      />
+    </div>
+  );
+};
+export default Items;
+
+export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
+
+  return {
+      paths: [], //indicates that no page needs be created at build time
+      fallback: 'blocking' //indicates the type of fallback
+  }
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  /*const url =
+    "https://script.google.com/macros/s/AKfycbwrtpxP95JWg2GghdxdxXDltiL101EWGhOGaJZZ0rRmedAow0t4hrQ4/exec";
+  // const res = await axios.get(url);
+  const res = await fetch(url);
+
+  const data = await res.json();
+  // const data = await res.data; */
+
+  return { props: { products: productData } };
+};
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+};
