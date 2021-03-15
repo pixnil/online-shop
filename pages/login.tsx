02d08eacedee3ab8a15ecce480a2658a35b27c51
Jay Shukla <jay.shukla@bolttech.io>
2021-03-15 13:08:30 +0800

    Added login page

---
+++ pages/login.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent } from "react";
+
+const Login: FunctionComponent = () => {
+  
+  // Login
+  const handleLogin = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // Authenticate user here
+  };
+
+  return (
+    <div className="container">
+      Login Form html will be here
+    </div>
+  );
+};
+
+export default Login;
